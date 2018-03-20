8a56e8b0ead288d0a6896a4f35ea4a0b48994cf1
Aman Jain <jainaman224@gmail.com>
2018-03-20 15:32:35 +0530

    branch fixes

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const db = require('../connections/mysqlConnection');
+const Sequelize = require('sequelize');
+const User = require('./User');
+
+const Product = db.define('product', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        required: true
+    },
+    description: {
+        type: Sequelize.STRING
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        required: true
+    },
+    image: {
+        type: Sequelize.STRING
+    }
+});
+
+Product.hasOne(User, {
+	foreignKey: 'user_id'
+});
+
+module.exports = Product;
