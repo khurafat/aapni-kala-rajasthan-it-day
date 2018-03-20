c0308555515d404077e7aecea5e1e77d2c74de3b
Piyush Agrawal <poush12@gmail.com>
2018-03-20 18:35:11 +0530

    wip

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index')
+	Product.findAll({
+		}).then((products) => {
+			return res.render('index', {products: products})
+		}).catch((error) => {
+			return response.json({
+				error: error
+			});
+	
   // res.sendFile(__dirname + '/../viewsindex.html');
 });
 
